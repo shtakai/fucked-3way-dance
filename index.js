a76be7015920e7ea18f1099bd234ffe99fa0bc0a
Alyson Takai <shtakai@gmail.com>
2019-11-21 16:28:48 +0900

    Add comparison of array

---
+++ index.js
@@ -0,0 +1,11 @@
+const s = 's0'
+const a = ['s1', 's2']
+
+x = typeof s === 'string' ? [s] : s
+y = typeof y === 'string' ? [a] : a
+
+console.log('x')
+console.log(x)
+console.log('xhamsters vs xvideos ===============')
+console.log('y')
+console.log(y)
---
+++ indexvideos.js
@@ -0,0 +1,13 @@
+const _ = require('lodash')
+
+const s = 's0'
+const a = ['s1', 's2']
+
+x = _.flatten([s])
+y = _.flatten([a])
+
+console.log('x')
+console.log(x)
+console.log('xhamsters vs xvideos ===============')
+console.log('y')
+console.log(y)
